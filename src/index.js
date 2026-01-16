b6ce899d4d7dfc23cbe93bc46dcc7ac42a59d9af
Evgeniy Galkovskiy <zhegal@gmail.com>
2026-01-17 00:05:19 +0200

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Express Demo API</h1><p>Status: Online</p>');
+    res.send('<h1>Express Demo API</h1><p>Status: Online</p><hr><div>It is working!!!</div>');
 });
 
 app.listen(PORT, () => {
