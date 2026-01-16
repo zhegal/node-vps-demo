948b4cbdb301c75edd2bd79412ddda978fd5e1c1
Evgeniy Galkovskiy <zhegal@gmail.com>
2026-01-16 23:51:10 +0200

    and so?

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Express Demo API</h1><p>Status: Online</p>');
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
