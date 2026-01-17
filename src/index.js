9de8bb225bdb5979724072be6b4f644066b8f2a9
Evgeniy Galkovskiy <zhegal@gmail.com>
2026-01-17 02:05:01 +0200

    timing

--- src/index.js
+++ src/index.js
@@ -3,19 +3,27 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send(`
-<h1>Express Demo API</h1>
+    res.send(`<h1>Express Demo API</h1>
 <p>Status: Online</p>
 <hr>
 <div>It is working!!!</div>
 <div id="a"></div>
 
 <script>
-const a = '11111';
-const b = document.getElementById('a');
-b.textContent = a;
-</script>
-    `);
+const a = document.getElementById('a');
+
+function printTime() {
+	const time = new Date();
+	const hours = String(time.getHours()).padStart(2, "0");
+	const minutes = String(time.getMinutes()).padStart(2, "0");
+	const seconds = String(time.getSeconds()).padStart(2, "0");
+    const result = ` + '`${hours}:${minutes}:${seconds}`' + `;
+    a.textContent = result;
+	setTimeout(printTime, 1000);
+}
+
+printTime();
+</script>`);
 });
 
 app.get('/test', (req, res) => {
